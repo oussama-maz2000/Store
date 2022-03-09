2bf1bf2ce2628daee0b8c7a1e305b7b6fb72dc54
Mazeghrane Oussama <loulououssama2015@gmail.com>
2022-03-09 23:08:12 +0100

    ...

--- Controllers/userAuthentication.js
+++ Controllers/userAuthentication.js
@@ -1,6 +1,7 @@
+require("dotenv").config();
 const route = require("express").Router();
 const userModel = require("../Models/userSchema");
-
+const jwt = require("jsonwebtoken");
 const {
   check_log_in,
   check_Sign_up,
@@ -17,12 +18,13 @@ route.post("/signup", async (req, res) => {
     // check email if exist in db
     let checkEmail = await userModel.findOne({ email: req.body.email });
     checkEmail && res.send("email exist before try with another one ");
+
     // create new user
     let user = new userModel({
       username: req.body.username,
       email: req.body.email,
       password: hash,
-      isadmin:req.body.isadmin
+      isadmin: req.body.isadmin,
     });
     //save it in db
     await user.save();
@@ -38,12 +40,24 @@ route.post("/login", async (req, res) => {
     let user = await userModel.findOne({ email: req.body.email });
     !user && res.send("user doesn't exist").status(400);
     let { error } = await check_log_in(req.body);
-    error && res.send("something wrong").status(401);
+    if (error) res.send("something wrong").status(401);
     let cmp = await compare(req.body.password, user.password);
     !cmp && res.send("wrong password try again please ").status(400);
-    res.send(`welcome ${user.username}`);
+    // create token
+
+    const access_token = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isadmin,
+      },
+      process.env.SECRET_JWT
+    );
+    const { password, ...others } = user._doc;
+
+// send data wihtout password
+    res.send({...others,access_token});
   } catch (err) {
-    res.send("something wrong").status(500);
+    res.send(err).status(500);
   }
 });
 module.exports = route;
