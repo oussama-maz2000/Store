e0c3c3e902c3a1703d63b81079cc33972094a905
Mazeghrane Oussama <loulououssama2015@gmail.com>
2022-03-09 23:08:40 +0100

    ...

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const express = require("express");
 const app = express();
 const connectDB = require("./Models/connection");
 const routeUser = require("./Controllers/userAuthentication");
+const usr=require('./Routers/User/userRoute')
 app.use(express.json());
 connectDB;
 app.listen(3000, console.log("server listening port 3000"));
 
 app.use("/user", routeUser);
-
+app.use('/usr',usr)
 app.get("/", (req, res) => {
   res.send("hello world");
 });
