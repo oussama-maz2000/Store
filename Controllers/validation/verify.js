7762b20908cd6b56d36bef0b77a83d19e37c7d5f
Mazeghrane Oussama <loulououssama2015@gmail.com>
2022-03-10 22:04:34 +0100

    ...

--- Controllers/validation/verify.js
+++ Controllers/validation/verify.js
@@ -2,16 +2,18 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 // middelware to check the token
 const verify_token = (req, res, next) => {
-  let authHeader = req.header.token;
-  if (authHeader) {
-    jwt.verify(authHeader, process.env.SECRET_JWT, (err, user) => {
-      if (err) return res.status(403).json({ err: "token not valide" });
-      req.user = user;
-      next();
-    });
-  } else {
-    return res.send(401).json({ err: "you're not authorized" });
+  let token = (req.headers || req.query || req.body.token)
+  //console.log(token);
+  if (!token.token)
+    return res.status(401).send("a token is required for authentication");
+  try {
+    let decode = jwt.verify(token, process.env.SECRET_JWT);
+    console.log(decode);
+    req.user = decode;
+  } catch (error) {
+    console.log("err", error.message);
   }
+  return next();
 };
 
 const verify_and_authorizate = (req, res, next) => {
