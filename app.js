fc766389d4dac6240802e9bf3569163e44b11d34
Mazeghrane Oussama <loulououssama2015@gmail.com>
2022-03-11 23:14:42 +0100

    ...

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ const express = require("express");
 const app = express();
 const connectDB = require("./Models/connection");
 const routeUser = require("./Controllers/userAuthentication");
-const {verify_token} = require("./Controllers/validation/verify");
+const store = require("./Controllers/Data/get_data");
+
 //const usr=require('./Routers/User/userRoute')
 app.use(express.json());
 connectDB;
 app.listen(3000, console.log("server listening port 3000"));
 
-app.use( routeUser);
+app.use(routeUser);
+app.use("/store", store);
 //app.use('/usr',usr)
-app.get("/", verify_token, (req, res) => {
+app.get("/", (req, res) => {
   res.send("welcome 🙌 ");
 });
