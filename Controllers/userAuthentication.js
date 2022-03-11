dcdc4aaa137fa4ee81f259bfa01b62d4b0299fc4
Mazeghrane Oussama <loulououssama2015@gmail.com>
2022-03-11 01:00:49 +0100

    ...

--- Controllers/userAuthentication.js
+++ Controllers/userAuthentication.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const route = require("express").Router();
 const userModel = require("../Models/userSchema");
+const { verify_token } = require("./validation/verify");
 const jwt = require("jsonwebtoken");
 const {
   check_log_in,
@@ -12,12 +13,13 @@ route.post("/signup", async (req, res) => {
   try {
     //check validation
     let { error } = await check_Sign_up(req.body);
-    error && console.log(error.details);
+    if (error) return res.status(401).send(error.details[0]);
     //hashing password
     let hash = await hashPassword(req.body.password);
     // check email if exist in db
     let checkEmail = await userModel.findOne({ email: req.body.email });
-    checkEmail && res.send("email exist before try with another one ");
+    if (checkEmail)
+      return res.status(400).send("email exist before try with another one ");
 
     // create new user
     let user = new userModel({
@@ -28,38 +30,48 @@ route.post("/signup", async (req, res) => {
     });
     //save it in db
     await user.save();
-    return res.send("thank you for registration");
+    return res.status(200).send("thank you for registration");
   } catch (err) {
     console.log(err.message);
-    return res.send(err.message);
-    process.exit(1);
+    return res.status(400).send(err.message);
   }
 });
 
 route.post("/login", async (req, res) => {
   try {
     let user = await userModel.findOne({ email: req.body.email });
-    !user && res.send("user doesn't exist").status(400);
+    if (!user) return res.status(400).send("user doesn't exist");
     let { error } = await check_log_in(req.body);
-    if (error) res.send("something wrong").status(401);
+    if (error) return res.status(401).send("something wrong");
     let cmp = await compare(req.body.password, user.password);
-    !cmp && res.send("wrong password try again please ").status(400);
+    !cmp &&
+      res.status(401).send("wrong password try again please ").status(400);
     // create token
+    if (user.isadmin == true) {
+      var access_token = jwt.sign(
+        {
+          id: user._id,
+          isAdmin: user.isadmin,
+        },
+        process.env.SECRET_JWT
+      );
+      const query = { username: user.username && isadmin};
+      const newdata = { token: "mazeghrane" };
+      //userModel.updateOne({username:user.username},{$set:{token:access_token}})
+      let dat=await userModel.findOneAndUpdate(query, newdata, {
+        new: true,
+      });
+      console.log(dat);
+    }
 
-   /*   const access_token = jwt.sign(
-      {
-        id: user._id,
-        isAdmin: user.isadmin,
-      },
-      process.env.SECRET_JWT
-    );  */
-    const { password, ...others } = user._doc;
-//console.log(access_token);
     // send data wihtout password
-    return res.send("fuck you");
+    return res.status(200).send("fuck you");
   } catch (err) {
-    return res.send(err).status(500);
-    process.exit(1);
+    return res.status(500).send(err.message);
   }
+});
+
+route.put("/:id", verify_token, (req, res) => {
+  res.send("welcome 🙌");
 });
 module.exports = route;
