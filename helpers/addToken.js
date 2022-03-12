a60b8c6a187c70b997dd884af10d4c8ae4905416
Mazeghrane Oussama <loulououssama2015@gmail.com>
2022-03-12 22:10:20 +0100

    ...

--- helpers/addToken.js
+++ helpers/addToken.js
@@ -1,20 +1,14 @@
 const Jwt = require("jsonwebtoken");
-const {userModel} = require("../Models/userSchema");
 require("dotenv").config();
 async function add_Token(admin) {
-  let access_token = Jwt.sign(
+  let access_token =  Jwt.sign(
     {
       _id: admin._id,
       isAdmin: admin.isadmin,
     },
     process.env.SECRET_JWT
   );
-  let query = { username: admin.username };
-  let data = { token: access_token };
-  let new_token = await userModel.findOneAndUpdate(query, data, {
-    new: true,
-  });
-  await new_token.save();
+  return  access_token;
 }
 
-module.exports={add_Token}
+module.exports = { add_Token };
