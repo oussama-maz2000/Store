3b161d5363e7009b6d5134945ea8816d30ea003e
Mazeghrane Oussama <loulououssama2015@gmail.com>
2022-03-12 22:10:03 +0100

    ...

--- Models/storeSchema.js
+++ Models/storeSchema.js
@@ -1,14 +1,16 @@
 const mongoose = require("mongoose");
 const storeSchema = new mongoose.Schema(
   {
-    id: { type: Number },
+    id: { type: Number, unique: true },
     title: { type: String, required: true },
     price: { type: Number, required: true },
+    size: [Number],
     category: { type: String },
     description: { type: String },
     image: { type: String },
+    available: { type: Boolean, default: true },
   },
   { timestamps: true }
 );
 const storeModel = mongoose.model("store", storeSchema);
-module.exports=storeModel
+module.exports = storeModel;
