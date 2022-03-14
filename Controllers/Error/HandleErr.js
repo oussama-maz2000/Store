cb424743ec3638cdd7f4c12c3ed595d4972707f0
Mazeghrane Oussama <loulououssama2015@gmail.com>
2022-03-14 23:02:58 +0100

    errors

--- Controllers/Error/HandleErr.js
+++ Controllers/Error/HandleErr.js
@@ -1,11 +1,19 @@
 class HandleError extends Error {
   statusCode;
   message;
-  status;
+
   constructor(statusCode, message) {
     super(message);
     this.statusCode = statusCode || 500;
     this.message = message || "fail";
   }
 }
-module.exports = HandleError;
+const catch_error = (req, res, next) => {
+  const err = new HandleError();
+  next(err);
+};
+
+const err_handle = (err, req, res, next) => {
+  res.status(err.statusCode).json({ err: err.message });
+};
+module.exports = { HandleError, err_handle, catch_error };
