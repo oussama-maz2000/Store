9def16b69ac60ac3b936e4191f46497216651993
Mazeghrane Oussama <loulououssama2015@gmail.com>
2022-03-18 23:30:18 +0100

    ...

--- app.js
+++ app.js
@@ -4,10 +4,7 @@ const compression = require("compression");
 const connectDB = require("./Models/connection");
 const routeUser = require("./Controllers/userAuthentication");
 const store = require("./Controllers/Data/get_data");
-const {
-  err_handle,
-  catch_error,
-} = require("./Controllers/Error/HandleErr");
+const { err_handle, catch_error } = require("./Controllers/Error/HandleErr");
 
 app.use(express.json());
 connectDB;
@@ -16,6 +13,10 @@ app.listen(3000, console.log("server listening port 3000"));
 app.use(compression());
 app.use(routeUser);
 app.use("/store", store);
+
+app.get("/", (req, res, next) => {
+  res.status(200).send("welcome");
+});
 app.use("*", (req, res, next) => {
   res.status(404).send("page not found");
 });
