241ea064aca499097de1f7165a41d9d92a153eca
Mazeghrane Oussama <loulououssama2015@gmail.com>
2022-03-22 22:23:21 +0100

    i added the catchAsync function

--- Controllers/Data/get_data.js
+++ Controllers/Data/get_data.js
@@ -2,35 +2,34 @@ const route = require("express").Router();
 const storeModel = require("../../Models/storeSchema");
 const { HandleError } = require("../Error/HandleErr");
 
-route.get("/", async (req, res, next) => {
-  try {
+const catchAsync = (fn) => {
+  return (req, res, next) => {
+    fn(req, res, next).catch(next);
+  };
+};
+route.get(
+  "/",
+  catchAsync(async (req, res, next) => {
     let data = await storeModel.find();
     return res.status(201).json({ data: data });
-  } catch (err) {
-    let error = new HandleError(401, "something wrong happend ");
-    next(error);
-  }
-});
-route.get("/expensive_product", async (req, res, next) => {
-  try {
+  })
+);
+route.get(
+  "/expensive_product",
+  catchAsync(async (req, res, next) => {
     let data = await storeModel.find({ price: { $gte: 150 } });
     return res.status(200).send(data);
     console.log(data);
-  } catch (err) {
-    let error = new HandleError(401, "something wrong happend");
-    next(error);
-  }
-});
-route.get("/cheap_product", async (req, res, next) => {
-  try {
+  })
+);
+route.get(
+  "/cheap_product",
+  catchAsync(async (req, res, next) => {
     let data = await storeModel.find({ price: { $lte: 150 } });
     return res.status(200).send(data);
     console.log(data);
-  } catch (err) {
-    let error = new HandleError(401, "something wrong happend");
-    next(error);
-  }
-});
+  })
+);
 route.post("/insertData", async (req, res, next) => {
   let client_data = req.body;
   try {
@@ -38,8 +37,7 @@ route.post("/insertData", async (req, res, next) => {
     await new_data.save();
     return res.status(201).send("good");
   } catch (err) {
-    let error = new HandleError(401, " something wrong happend ");
-    next(error);
+    res.status(400).send(err.message);
   }
 });
 
@@ -48,8 +46,9 @@ route.get("/Shoes", async (req, res, next) => {
     let data = await storeModel.find({ category: "Shoes" });
     return res.status(201).send(data);
   } catch (err) {
-    let error = new HandleError(401, err.message);
-    next(error);
+    console.log(err.message);
+
+    res.status(400).send(err.message);
   }
 });
 route.get("/T-shirt", async (req, res, next) => {
@@ -57,32 +56,29 @@ route.get("/T-shirt", async (req, res, next) => {
     let data = await storeModel.find({ category: "T-shirt" });
     return res.status(201).send(data);
   } catch (err) {
-    let error = new HandleError(401, "something wrong happend ");
-    next(error);
+    res.status(400).send(err.message);
   }
 });
 
-route.get("/jeans", async (req, res, next) => {
-  try {
+route.get(
+  "/jeans",
+  catchAsync(async (req, res, next) => {
     let data = await storeModel.find({ category: "Jeans" });
     return res.status(201).send(data);
-  } catch (err) {
-    let error = new HandleError(401, "something wrong happend ");
-    next(error);
-  }
-});
+  })
+);
 
 route.get("/:id", async (req, res, next) => {
+
   try {
     let data = await storeModel.findOne(req.params);
     if (data) {
       res.status(201).send(data);
     } else {
-      res.status(401).send("sorry we don't have it ");
+      next(new HandleError(`${req.params.id} doesn't exist`, 401));
     }
   } catch (err) {
-    let error = new HandleError(401, "something wrong happend ");
-    next(error);
+    res.status(400).send(err);
   }
 });
 route.delete("/:id", async (req, res, next) => {
@@ -91,8 +87,7 @@ route.delete("/:id", async (req, res, next) => {
     const delete_product = await storeModel.deleteOne(req.params);
     res.status(200).send("deleting successfully");
   } catch (err) {
-    let error = new HandleError(301, "try again please");
-    next(error);
+    res.status(400).send(err.message);
   }
 });
 route.patch("/:id", async (req, res, next) => {
@@ -103,8 +98,8 @@ route.patch("/:id", async (req, res, next) => {
     res.status(200).send("update successfully");
   } catch (err) {
     console.log(err.message);
-    let error = new HandleError(301, "you have problem");
-    next(error);
+
+    res.status(400).send(err.message);
   }
 });
 
