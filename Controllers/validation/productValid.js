59aa6beaa862221cc061e64433ccc21bd18e065c
Mazeghrane Oussama <loulououssama2015@gmail.com>
2022-03-24 21:46:43 +0100

    ...

--- Controllers/validation/productValid.js
+++ Controllers/validation/productValid.js
@@ -3,14 +3,26 @@ const joi = require("joi");
 const productVerify = async (data) => {
   const schema = joi.object({
     id: joi.number().required(),
-    title: joi.alphanum().min(5).required(),
+    title: joi.string().min(5).required(),
     price: joi.number().required(),
-    category: joi.string().required(),
-    description: joi.string(),
-    image: joi.string().required(),
+    category: joi.string().required().min(5),
+    description: joi.string().min(5),
+    image: joi.string().required().min(5),
     available: joi.boolean(),
   });
-  return await schema.validate(data);
+  return schema.validate(data);
 };
 
-module.exports = { productVerify };
+const update_product_Verify = async (data) => {
+  const schema = joi.object({
+    title: joi.string().min(5),
+    price: joi.number(),
+    category: joi.string(),
+    description: joi.string(),
+    image: joi.string(),
+    available: joi.boolean(),
+  });
+  return schema.validate(data);
+};
+
+module.exports = { productVerify, update_product_Verify };
