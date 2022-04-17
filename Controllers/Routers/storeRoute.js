d6ecccddcb9937e5fea02c328d9b803df400d50c
Mazeghrane Oussama <loulououssama2015@gmail.com>
2022-04-17 16:51:38 +0100

    have error in update function

--- Controllers/Routers/storeRoute.js
+++ Controllers/Routers/storeRoute.js
@@ -26,7 +26,7 @@ const insert_product = async (req, res, next) => {
   if (error) return next(new HandleError(error.details[0].message, 402));
   let new_data = await new storeModel(req.body);
   await new_data.save();
-  return res.status(201).send("good");
+  return res.status(201).send("inserting successfully ...");
 };
 
 const get_Shoes = async (req, res, next) => {
@@ -50,26 +50,50 @@ const product_byID = async (req, res, next) => {
     return next(new HandleError(`no product with id =${req.params.id}`));
   res.status(200).json({ data });
 };
-const deleteOne=async (req, res, next) => {
-    const id = req.params.id;
-    const element_deleting = await storeModel.findOne(id);
-    if (!element_deleting)
-      return next(new HandleError(`id =${id} doesn't exist`));
-    else await storeModel.deleteOne(id);
-    res.status(200).send("deleting successfully");
-  };
-route.patch(
-  "/:id",
-  catchAsync(async (req, res, next) => {
-    let body = req.body;
-    let { id } = req.params * 1;
-    const { error } = await update_product_Verify(body);
-    if (error) return next(new HandleError(error.details[0].message, 401));
-    else {
-      await storeModel.findByIdAndUpdate(id, { $set: body });
-      res.status(200).send("update successfully");
-    }
-  })
-);
+const deleteOne = async (req, res, next) => {
+  const id = req.params.id * 1;
+  const element_deleting = await storeModel.findOne({ id });
+  if (!element_deleting)
+    return next(new HandleError(`id =${id} doesn't exist`));
+  else {
+    let deleting = await storeModel.deleteOne({ id });
+  }
+  res.status(200).send("deleting successfully ...");
+};
+const update = async (req, res, next) => {
+  let body = req.body;
+  //console.log(body);
+  const id = req.params.id * 1;
+  const element_updating = await storeModel.findOne({ id });
+  console.log(element_updating);
+  const { error } = await update_product_Verify(body);
+  if (error) return next(new HandleError(error.details[0].message, 401));
+  /*  else {
+    await storeModel.findByIdAndUpdate(
+      _id ,
+      {
+        title: req.body.title,
+        price: req.body.price,
+        size: req.body.size,
+        category: req.body.category,
+        description: req.body.description,
+        image: req.body.image,
+        available: req.body.available,
+      }
+    );
+  }
+  */ res.status(200).send("update successfully ...");
+};
 
-module.exports = { get_All };
+module.exports = {
+  get_All,
+  expensive_product,
+  cheap_product,
+  insert_product,
+  get_Shoes,
+  get_Tshirt,
+  get_jeans,
+  product_byID,
+  deleteOne,
+  update,
+};
