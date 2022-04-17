8c9de836d895c7bf52894cc757f277111f923d80
Mazeghrane Oussama <loulououssama2015@gmail.com>
2022-04-17 20:07:21 +0100

    ...

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require("express");
-
 const app = express();
 const compression = require("compression");
 const connection = require("./Models/connection");
-const router=require('./Controllers/authentication/userAuthentication')
-//const store = require("./Controllers/Routers/storeRoute");
+const user = require("./Controllers/generators/userGEN");
 const store = require("./Controllers/generators/storeGEN");
 
 const { HandleError, golobaleEroor } = require("./Controllers/Error/HandleErr");
@@ -15,13 +13,13 @@ app.listen(3001, console.log("server listening port 3001"));
 
 app.use(compression());
 
+app.use("/usr", user);
 app.use("/store", store);
-app.use(router)
 app.get("/", (req, res, next) => {
   res.status(200).send("welcome");
 });
-app.all("*", (req, res, next) => {
+/* app.get("*", (req, res, next) => {
   next(new HandleError(`couldn't find ${req.originalUrl}`, 400));
-});
+}); */
 
 app.use(golobaleEroor);
