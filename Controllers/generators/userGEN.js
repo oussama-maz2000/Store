1ed1c4acc3abb5c2a2fe58ff56a9a5b21f48d7f4
Mazeghrane Oussama <loulououssama2015@gmail.com>
2022-04-21 20:27:39 +0100

    add /forgot router

--- Controllers/generators/userGEN.js
+++ Controllers/generators/userGEN.js
@@ -4,9 +4,11 @@ const {
   login,
   sign,
   protect,
-
+  forgetPassword
 } = require("../authentication/userAuthentication");
 router.route("/sign").post(sign);
 router.route("/login").post(login);
+router.route("/forgot").post(forgetPassword);
+
 
 module.exports = router;
