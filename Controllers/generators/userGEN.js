39df6e767fe724976daadb5f47db79860084ca1c
Mazeghrane Oussama <loulououssama2015@gmail.com>
2022-04-27 15:55:45 +0100

    done

--- Controllers/generators/userGEN.js
+++ Controllers/generators/userGEN.js
@@ -4,11 +4,12 @@ const {
   login,
   sign,
   protect,
-  forgetPassword
+  forgetPassword,
+  resetPassword,
 } = require("../authentication/userAuthentication");
 router.route("/sign").post(sign);
 router.route("/login").post(login);
 router.route("/forgot").post(forgetPassword);
-
+router.route("/reset").patch(resetPassword);
 
 module.exports = router;
