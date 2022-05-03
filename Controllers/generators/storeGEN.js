f66ea335f1d9f684eb07013058edfd5a30139f21
Mazeghrane Oussama <loulououssama2015@gmail.com>
2022-05-03 15:02:45 +0100

    ...

--- Controllers/generators/storeGEN.js
+++ Controllers/generators/storeGEN.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { protect,restrict } = require("../authentication/userAuthentication");
+const { protect, restrict } = require("../authentication/userAuthentication");
 
 const {
   get_All,
@@ -10,15 +10,18 @@ const {
   get_jeans,
   product_byID,
   deleteOne,
-  update,
+  update_Data,
 } = require("../Routers/storeRoute");
 
-router.route("/").get(protect,get_All);
-router.route("/insert").post(protect,restrict(),insert_product);
-router.route("/:id").delete(protect,restrict(),deleteOne).patch(protect,restrict(),update).get(product_byID);
+router.route("/").get(protect, get_All);
+router.route("/insert").post(protect, restrict(), insert_product);
+router
+  .route("/:id")
+  .delete(protect, restrict(), deleteOne)
+  .patch(protect, restrict(), update_Data)
+  .get(product_byID);
 router.route("/shoes").get(get_Shoes);
 router.route("/jeans").get(get_jeans);
 router.route("/tshirt").get(get_Tshirt);
 
 module.exports = router;
-
--- Controllers/generators/userGEN.js
+++ Controllers/generators/userGEN.js
@@ -8,10 +8,12 @@ const {
   resetPassword,
   updatePassword,
 } = require("../authentication/userAuthentication");
+const { updateMe } = require("../authentication/userController");
 router.route("/sign").post(sign);
 router.route("/login").post(login);
-router.route("/forgot").post(forgetPassword);
-router.route("/reset").patch(resetPassword);
-router.route("/update").patch(updatePassword);
+router.route("/forgotPassword").post(forgetPassword);
+router.route("/resetPassword").patch(resetPassword);
+router.route("/updateMyPassword").patch(updatePassword);
+router.route("/updateMe").patch(protect, updateMe);
 
 module.exports = router;
