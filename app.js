19f809ef217308fd35a5f75e2aed6691b873e06d
Mazeghrane Oussama <loulououssama2015@gmail.com>
2022-05-06 01:12:48 +0100

    add middlware for serurity

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ const compression = require("compression");
 const connection = require("./Models/connection");
 const user = require("./Controllers/generators/userGEN");
 const store = require("./Controllers/generators/storeGEN");
-
+const ratelimiter = require("express-rate-limit");
+const sanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
 const { HandleError, golobaleEroor } = require("./Controllers/Error/HandleErr");
+// securite
+app.use(helmet());
 connection;
 app.use(express.json());
 app.use(
@@ -14,7 +19,19 @@ app.use(
   })
 );
 
-app.listen(3001, console.log("server listening port 3001"));
+//how to impliment rate limiter
+const limiter = ratelimiter({
+  max: 50,
+  windowMs: 60 * 1000,
+  message: "too many requests from this IP, try again after one hour🤪",
+});
+app.use("/store", limiter);
+//data sanitization against nosql query injection
+app.use(sanitize());
+
+//data sanitization against xss
+app.use(xss());
+app.listen(3001, console.log("server listening port 3001 😎"));
 
 app.use(compression());
 
