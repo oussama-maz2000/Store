58bf6cdc1c2669e0a18a0726de77d01f88375cf8
Mazeghrane Oussama <loulououssama2015@gmail.com>
2022-05-16 22:08:49 +0100

    ...

---
+++ Models/orderSchema.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const orderSchema = new mongoose.Schema({
+  //userID: { type: mongoose.Schema.ObjectId, ref: "usermodel" },
+  productId: { type: mongoose.Schema.ObjectId, ref: "store" },
+  total: { type: Number, required: true },
+  qty: { type: Number, required: true },
+});
+
+const userModel = mongoose.model("orders", orderSchema);
